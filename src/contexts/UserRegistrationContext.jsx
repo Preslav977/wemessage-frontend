882baf27cc4df8e5dba1e8c42bf382046b94a311
Preslav977 <p.cvetanow@gmail.com>
2025-01-20 09:28:52 +0200

    Created and moved the contexts in different file, since React was showing an error with fast refreshing

---
+++ src/contexts/UserRegistrationContext.jsx
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+
+export const FirstNameContext = createContext(null);
+
+export const LastNameContext = createContext(null);
+
+export const UserNameContext = createContext(null);
+
+export const PasswordContext = createContext(null);
+
+export const ConfirmPasswordContext = createContext(null);
+
+export const BioContext = createContext(null);
+
+export const ProfilePictureContext = createContext(null);
+
+export const BackgroundPictureContext = createContext(null);
