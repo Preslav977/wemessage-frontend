32d65c5e67eeea1339c25ff355ae16c9f8f3eb1c
Preslav977 <p.cvetanow@gmail.com>
2025-01-22 09:37:34 +0200

    Sign up form works for creating a user next is to create live validation

--- src/components/SignUpFormComponent.jsx
+++ src/components/SignUpFormComponent.jsx
@@ -47,8 +47,24 @@ function SignUpFormComponent() {
     e.preventDefault();
 
     const FormDataUserObject = new FormData(e.target);
+    const firstName = FormDataUserObject.get("first_name");
+    const lastName = FormDataUserObject.get("last_name");
+    const username = FormDataUserObject.get("username");
+    const password = FormDataUserObject.get("password");
+    const confirmPassword = FormDataUserObject.get("confirm_password");
 
     console.log(FormDataUserObject);
+
+    const signUpAndCreateUser = {
+      ...userSignUpObj,
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+
+    setUserSignUpObj(signUpAndCreateUser);
 
     try {
       const response = await fetch("http://localhost:5000/users/signup", {
@@ -57,7 +73,11 @@ function SignUpFormComponent() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          //
+          first_name: firstName,
+          last_name: lastName,
+          username: username,
+          password: password,
+          confirm_password: confirmPassword,
         }),
       });
 
@@ -82,35 +102,78 @@ function SignUpFormComponent() {
         </div>
         <h1 className={styles.formHeader}>Sign up</h1>
         <div className={styles.formFlexedContainer}>
-          <form className={styles.formContainer}>
+          <form className={styles.formContainer} onSubmit={handleSubmit}>
             <div className={styles.formFlexedContainer}>
               <div className={styles.formContent}>
-                <label htmlFor="">First name:</label>
-                <input type="text" name="" id="" />
+                <label htmlFor="first_name">First name:</label>
+                <input
+                  type="text"
+                  name="first_name"
+                  id="first_name"
+                  minLength={1}
+                  maxLength={30}
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
               </div>
 
               <div className={styles.formContent}>
-                <label htmlFor="">Last name:</label>
-                <input type="text" name="" id="" />
+                <label htmlFor="last_name">Last name:</label>
+                <input
+                  type="text"
+                  name="last_name"
+                  id="last_name"
+                  minLength={1}
+                  maxLength={30}
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
               </div>
             </div>
-
             <div className={styles.formContent}>
-              <label htmlFor="">Username:</label>
-              <input type="text" name="" id="" />
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                minLength={1}
+                maxLength={30}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
             </div>
 
             <div className={styles.formContent}>
-              <label htmlFor="">Password:</label>
-              <input type="password" name="" id="" />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                minLength={8}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
             </div>
 
             <div className={styles.formContent}>
-              <label htmlFor="">Confirm password:</label>
-              <input type="password" name="" id="" />
+              <label htmlFor="confirm_password">Confirm password:</label>
+              <input
+                type="password"
+                name="confirm_password"
+                id="confirm_password"
+                minLength={8}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
             </div>
             <p className={styles.loginAnchorLink}>
-              Already have an account? <a href="/login">Log In</a>
+              Already have an account?{" "}
+              <Link to="http://localhost:5000/users/login">Log In</Link>
             </p>
             <div className={styles.submitBtnContainer}>
               <button className={styles.submitBtn}>Submit</button>
@@ -120,7 +183,7 @@ function SignUpFormComponent() {
             <img
               className={styles.formRightSideFlexedImage}
               src="wemessage.jpg"
-              alt=""
+              alt="wemessage"
             />
           </div>
         </div>
