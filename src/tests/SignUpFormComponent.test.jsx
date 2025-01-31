420825f5f13ab011937bef6c4ff437956f9a1986
Preslav977 <p.cvetanow@gmail.com>
2025-01-31 10:30:38 +0200

    Test for sign up form to render log in form when anchor link is clicked

--- src/tests/SignUpFormComponent.test.jsx
+++ src/tests/SignUpFormComponent.test.jsx
@@ -7,12 +7,12 @@ import { createMemoryRouter, RouterProvider } from "react-router-dom";
 describe("should render SignUpFormComponent", () => {
   it("should render the content of this component", () => {
     const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
+      initialEntries: ["/signup"],
     });
 
     render(<RouterProvider router={router} />);
 
-    screen.debug();
+    // screen.debug();
 
     expect(screen.queryByText("Wemessage").textContent).toMatch(/wemessage/i);
 
@@ -43,12 +43,12 @@ describe("should render SignUpFormComponent", () => {
 
   it("should render content and span errors", () => {
     const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
+      initialEntries: ["/signup"],
     });
 
     render(<RouterProvider router={router} />);
 
-    screen.debug();
+    // screen.debug();
 
     expect(screen.queryByText("Wemessage").textContent).toMatch(/wemessage/i);
 
@@ -102,12 +102,12 @@ describe("should render SignUpFormComponent", () => {
 
   it("should hide span errors when user types", async () => {
     const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
+      initialEntries: ["/signup"],
     });
 
     render(<RouterProvider router={router} />);
 
-    screen.debug();
+    // screen.debug();
 
     const user = userEvent.setup();
 
@@ -150,5 +150,48 @@ describe("should render SignUpFormComponent", () => {
     expect(screen.getByRole("confirm_password")).toHaveValue("12345678Bg@");
 
     expect(screen.queryByText("Password must match")).not.toBeInTheDocument();
+  });
+
+  it("should navigate to login form when link is clicked", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/signup", "/login"],
+      initialIndex: 0,
+    });
+
+    render(<RouterProvider router={router} />);
+
+    screen.debug();
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByText("Log In"));
+
+    screen.debug();
+
+    expect(screen.getByText("Wemessage").textContent).toMatch(/wemessage/i);
+
+    expect(screen.queryByText("Log in").textContent).toMatch(/log in/i);
+
+    expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
+
+    expect(
+      screen.queryByText("Username is required to log in").textContent,
+    ).toMatch(/username is required to log in/i);
+
+    expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
+
+    expect(
+      screen.queryByText("Password is required to log in").textContent,
+    ).toMatch(/password is required to log in/i);
+
+    expect(screen.queryByText("Don't have an account?").textContent).toMatch(
+      /don't have an account?/i,
+    );
+
+    expect(screen.queryByText("Sign Up").textContent).toMatch(/sign up/i);
+
+    expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
+
+    expect(screen.queryAllByRole("button")[1], { name: "Submit" });
   });
 });
