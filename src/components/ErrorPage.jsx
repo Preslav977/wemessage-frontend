2e4599da31ce0b2a06713a53e3dd09e6ab3809ad
Preslav977 <p.cvetanow@gmail.com>
2025-02-09 10:56:46 +0200

    Implemented Error component to render an error when user goes into not matched route in the router

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 function ErrorPage() {
+  const error = useRouteError();
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/login");
+    setTimeout(() => {
+      navigate("/login");
+    }, 3000);
   });
 
-  return <p>123</p>;
+  return (
+    <div>
+      <h4>Oops!</h4>
+      <p>Sorry, an unexpected error has occurred!</p>
+      <p>{error.statusText || error.message}</p>
+      <p>You will be redirected to login form in 3 seconds!</p>
+    </div>
+  );
 }
 
 export default ErrorPage;
