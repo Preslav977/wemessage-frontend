a371bf0d7f342abbbda9ebda33541cd7f0027030
Preslav977 <p.cvetanow@gmail.com>
2025-02-21 10:42:42 +0200

    Everything seems to be passing for now. I can move on with other tests

--- src/tests/LogInForm.test.jsx
+++ src/tests/LogInForm.test.jsx
@@ -20,71 +20,71 @@ afterAll(() => {
 });
 
 describe("should render LogInForm", () => {
-  // it("should render the content of this component", () => {
-  //   const router = createMemoryRouter(routes, {
-  //     initialEntries: ["/login"],
-  //   });
+  it("should render the content of this component", () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login"],
+    });
 
-  //   render(<RouterProvider router={router} />);
+    render(<RouterProvider router={router} />);
 
-  //   // screen.debug();
+    // screen.debug();
 
-  //   expect(screen.getByText("Wemessage").textContent).toMatch(/wemessage/i);
+    expect(screen.getByText("Wemessage").textContent).toMatch(/wemessage/i);
 
-  //   expect(screen.queryByText("Log in").textContent).toMatch(/log in/i);
+    expect(screen.queryByText("Log in").textContent).toMatch(/log in/i);
 
-  //   expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
+    expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
 
-  //   expect(
-  //     screen.queryByText("Username is required to log in").textContent,
-  //   ).toMatch(/username is required to log in/i);
+    expect(
+      screen.queryByText("Username is required to log in").textContent,
+    ).toMatch(/username is required to log in/i);
 
-  //   expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
+    expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
 
-  //   expect(
-  //     screen.queryByText("Password is required to log in").textContent,
-  //   ).toMatch(/password is required to log in/i);
+    expect(
+      screen.queryByText("Password is required to log in").textContent,
+    ).toMatch(/password is required to log in/i);
 
-  //   expect(screen.queryByText("Don't have an account?").textContent).toMatch(
-  //     /don't have an account?/i,
-  //   );
+    expect(screen.queryByText("Don't have an account?").textContent).toMatch(
+      /don't have an account?/i,
+    );
 
-  //   expect(screen.queryByText("Sign Up").textContent).toMatch(/sign up/i);
+    expect(screen.queryByText("Sign Up").textContent).toMatch(/sign up/i);
 
-  //   expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
+    expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
 
-  //   expect(screen.queryAllByRole("button")[1], { name: "Submit" });
-  // });
+    expect(screen.queryAllByRole("button")[1], { name: "Submit" });
+  });
 
-  // it("should hide span errors when user types", async () => {
-  //   const router = createMemoryRouter(routes, {
-  //     initialEntries: ["/login"],
-  //   });
+  it("should hide span errors when user types", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login"],
+    });
 
-  //   render(<RouterProvider router={router} />);
+    render(<RouterProvider router={router} />);
 
-  //   const user = userEvent.setup();
+    const user = userEvent.setup();
 
-  //   await user.type(screen.getByTestId("username"), "preslaw");
+    await user.type(screen.getByTestId("username"), "preslaw");
 
-  //   expect(screen.getByTestId("username")).toHaveValue("preslaw");
+    expect(screen.getByTestId("username")).toHaveValue("preslaw");
 
-  //   await user.type(screen.getByTestId("password"), "12345678Bg@");
+    await user.type(screen.getByTestId("password"), "12345678Bg@");
 
-  //   expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
+    expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
 
-  //   expect(
-  //     screen.queryByText("Username is required to log in"),
-  //   ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("Username is required to log in"),
+    ).not.toBeInTheDocument();
 
-  //   expect(
-  //     screen.queryByText("Password is required to log in"),
-  //   ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText("Password is required to log in"),
+    ).not.toBeInTheDocument();
 
-  //   expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
+    expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
 
-  //   expect(screen.queryAllByRole("button")[1], { name: "Submit" });
-  // });
+    expect(screen.queryAllByRole("button")[1], { name: "Submit" });
+  });
 
   it("should render loading spinner when guest login button is clicked", async () => {
     const router = createMemoryRouter(routes, {
@@ -92,21 +92,6 @@ describe("should render LogInForm", () => {
     });
 
     render(<RouterProvider router={router} />);
-
-    // server.use(
-    //   http.get("http://localhost:5000/users", () => {
-    //     return HttpResponse.json(
-    //       {
-    //         id: 4,
-    //         first_name: "preslaw",
-    //         last_name: "preslaw",
-    //         username: "preslaw",
-    //         bio: "",
-    //       },
-    //       { status: 200 },
-    //     );
-    //   }),
-    // );
 
     const user = userEvent.setup();
 
@@ -247,93 +232,93 @@ describe("should render LogInForm", () => {
     expect(unauthorizedErr).toBeInTheDocument();
   });
 
-  // it("should redirect and render to sign up form when anchor link clicked", async () => {
-  //   const router = createMemoryRouter(routes, {
-  //     initialEntries: ["/login", "/signup"],
-  //     initialIndex: 0,
-  //   });
+  it("should redirect and render to sign up form when anchor link clicked", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/signup"],
+      initialIndex: 0,
+    });
 
-  //   render(<RouterProvider router={router} />);
+    render(<RouterProvider router={router} />);
 
-  //   const user = userEvent.setup();
+    const user = userEvent.setup();
 
-  //   expect(screen.getByText("Wemessage").textContent).toMatch(/wemessage/i);
+    expect(screen.getByText("Wemessage").textContent).toMatch(/wemessage/i);
 
-  //   expect(screen.queryByText("Log in").textContent).toMatch(/log in/i);
+    expect(screen.queryByText("Log in").textContent).toMatch(/log in/i);
 
-  //   expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
+    expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
 
-  //   expect(
-  //     screen.queryByText("Username is required to log in").textContent,
-  //   ).toMatch(/username is required to log in/i);
+    expect(
+      screen.queryByText("Username is required to log in").textContent,
+    ).toMatch(/username is required to log in/i);
 
-  //   expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
+    expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
 
-  //   expect(
-  //     screen.queryByText("Password is required to log in").textContent,
-  //   ).toMatch(/password is required to log in/i);
+    expect(
+      screen.queryByText("Password is required to log in").textContent,
+    ).toMatch(/password is required to log in/i);
 
-  //   expect(screen.queryByText("Don't have an account?").textContent).toMatch(
-  //     /don't have an account?/i,
-  //   );
+    expect(screen.queryByText("Don't have an account?").textContent).toMatch(
+      /don't have an account?/i,
+    );
 
-  //   expect(screen.queryByText("Sign Up").textContent).toMatch(/sign up/i);
+    expect(screen.queryByText("Sign Up").textContent).toMatch(/sign up/i);
 
-  //   expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
+    expect(screen.queryAllByRole("button")[0], { name: "Guest Login" });
 
-  //   expect(screen.queryAllByRole("button")[1], { name: "Submit" });
+    expect(screen.queryAllByRole("button")[1], { name: "Submit" });
 
-  //   await user.click(screen.queryByText("Sign Up"));
+    await user.click(screen.queryByText("Sign Up"));
 
-  //   // screen.debug();
+    // screen.debug();
 
-  //   expect(screen.queryByText("Wemessage").textContent).toMatch(/wemessage/i);
+    expect(screen.queryByText("Wemessage").textContent).toMatch(/wemessage/i);
 
-  //   expect(screen.queryByRole("h1"), { name: "Sign up" });
+    expect(screen.queryByRole("h1"), { name: "Sign up" });
 
-  //   expect(screen.queryByText("First name:").textContent).toMatch(
-  //     /first name:/i,
-  //   );
+    expect(screen.queryByText("First name:").textContent).toMatch(
+      /first name:/i,
+    );
 
-  //   expect(
-  //     screen.queryByText("First name must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/first name must be between 1 and 30 characters/i);
+    expect(
+      screen.queryByText("First name must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/first name must be between 1 and 30 characters/i);
 
-  //   expect(
-  //     screen.queryByText("Last name must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/last name must be between 1 and 30 characters/i);
+    expect(
+      screen.queryByText("Last name must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/last name must be between 1 and 30 characters/i);
 
-  //   expect(
-  //     screen.queryByText("Username must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/username must be between 1 and 30 characters/i);
+    expect(
+      screen.queryByText("Username must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/username must be between 1 and 30 characters/i);
 
-  //   expect(
-  //     screen.queryByText(
-  //       "Password must be 8 characters long, and contain one lower, one uppercase and one special character",
-  //     ).textContent,
-  //   ).toMatch(
-  //     /password must be 8 characters long, and contain one lower, one uppercase and one special character/i,
-  //   );
+    expect(
+      screen.queryByText(
+        "Password must be 8 characters long, and contain one lower, one uppercase and one special character",
+      ).textContent,
+    ).toMatch(
+      /password must be 8 characters long, and contain one lower, one uppercase and one special character/i,
+    );
 
-  //   expect(screen.queryByText("Last name:").textContent).toMatch(/last name:/i);
+    expect(screen.queryByText("Last name:").textContent).toMatch(/last name:/i);
 
-  //   expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
+    expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
 
-  //   expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
+    expect(screen.queryByText("Password:").textContent).toMatch(/password:/i);
 
-  //   expect(screen.queryByText("Confirm Password:").textContent).toMatch(
-  //     /confirm password:/i,
-  //   );
+    expect(screen.queryByText("Confirm Password:").textContent).toMatch(
+      /confirm password:/i,
+    );
 
-  //   expect(screen.queryByText("Already have an account?").textContent).toMatch(
-  //     /already have an account?/i,
-  //   );
+    expect(screen.queryByText("Already have an account?").textContent).toMatch(
+      /already have an account?/i,
+    );
 
-  //   expect(screen.queryByText("Log In").textContent).toMatch(/log in/i);
+    expect(screen.queryByText("Log In").textContent).toMatch(/log in/i);
 
-  //   expect(screen.queryByRole("button"), { name: "Submit" });
-  // });
+    expect(screen.queryByRole("button"), { name: "Submit" });
+  });
 });
--- src/tests/MainGridInterface.test.jsx
+++ src/tests/MainGridInterface.test.jsx
@@ -24,148 +24,210 @@ afterAll(() => {
 });
 
 describe("should render MainGridInterface", () => {
-  // it("should render Login form then navigate to UserProfile", async () => {
-  //   const router = createMemoryRouter(routes, {
-  //     initialEntries: ["/login", "/profile/4"],
-  //     initialIndex: 0,
-  //   });
-  //   // server.use(
-  //   //   http.get("http://localhost:5000/users", () => {
-  //   //     return HttpResponse.json(
-  //   //       {
-  //   //         id: 4,
-  //   //         first_name: "preslaw",
-  //   //         last_name: "preslaw",
-  //   //         username: "preslaw",
-  //   //         bio: "",
-  //   //       },
-  //   //       { status: 200 },
-  //   //     );
-  //   //   }),
-  //   // );
-  //   render(<RouterProvider router={router} />);
-  //   const user = userEvent.setup();
-  //   await user.type(screen.getByTestId("username"), "preslaw");
-  //   expect(screen.getByTestId("username")).toHaveValue("preslaw");
-  //   await user.type(screen.getByTestId("password"), "12345678Bg@");
-  //   expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
-  //   const submitBtn = screen.queryAllByRole("button");
-  //   // screen.debug();
-  //   await user.click(submitBtn[1]);
-  //   await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+  it("should render Login form then navigate to UserProfile", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/profile/4"],
+      initialIndex: 0,
+    });
 
-  //   // screen.debug();
+    server.use(
+      http.get("http://localhost:5000/users", () => {
+        return HttpResponse.json(
+          {
+            id: 4,
+            first_name: "preslaw",
+            last_name: "preslaw",
+            username: "preslaw",
+            password: "12345678Bg@",
+            confirm_password: "12345678Bg@",
+            bio: "",
+          },
+          { status: 200 },
+        );
+      }),
+    );
 
-  //   expect(screen.queryByText("Global").textContent).toMatch(/global/i);
-  //   expect(screen.queryByText("Chats").textContent).toMatch(/chats/i);
-  //   expect(screen.queryByText("Groups").textContent).toMatch(/groups/i);
-  //   expect(screen.queryAllByText("Profile")[0].textContent).toMatch(/profile/i);
-  //   expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
-  //   expect(screen.queryByText("Manage Profile").textContent).toMatch(
-  //     /manage profile/i,
-  //   );
-  //   expect(screen.queryAllByText("Profile")[1].textContent).toMatch(/profile/i);
-  //   expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
-  //     /edit profile/i,
-  //   );
-  //   expect(screen.queryByText("Change Password").textContent).toMatch(
-  //     /change password/i,
-  //   );
-  //   // expect(screen.queryByRole("button", { name: "Save" })).toBeInTheDocument();
-  //   expect(screen.queryByText("preslaw preslaw").textContent).toMatch(
-  //     /preslaw preslaw/i,
-  //   );
-  //   expect(screen.queryByText("@preslaw").textContent).toMatch(/@preslaw/);
-  //   expect(screen.queryByTestId("user_presence")).toBeInTheDocument();
-  //   expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
-  //     /edit profile/i,
-  //   );
-  // });
-  // it("should render Login form then navigate to EditUserProfile", async () => {
-  //   const router = createMemoryRouter(routes, {
-  //     initialEntries: ["/login", "/profile/4", "/profile/edit/4"],
-  //     initialIndex: 0,
-  //   });
-  //   // server.use(
-  //   //   http.get("http://localhost:5000/users", () => {
-  //   //     return HttpResponse.json(
-  //   //       {
-  //   //         id: 4,
-  //   //         first_name: "preslaw",
-  //   //         last_name: "preslaw",
-  //   //         username: "preslaw",
-  //   //         bio: "",
-  //   //       },
-  //   //       { status: 200 },
-  //   //     );
-  //   //   }),
-  //   // );
-  //   render(<RouterProvider router={router} />);
-  //   const user = userEvent.setup();
-  //   await user.type(screen.getByTestId("username"), "preslaw");
-  //   expect(screen.getByTestId("username")).toHaveValue("preslaw");
-  //   await user.type(screen.getByTestId("password"), "12345678Bg@");
-  //   expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
-  //   const submitBtn = screen.queryAllByRole("button");
-  //   // screen.debug();
-  //   await user.click(submitBtn[1]);
-  //   // const loadingBtn = await screen.findByTestId("loading-btn");
-  //   // expect(loadingBtn).toBeInTheDocument();
-  //   await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
-  //   expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
-  //     /edit profile/i,
-  //   );
-  //   const manageUserProfileEditProfile = screen.queryAllByText("Edit Profile");
-  //   await user.click(manageUserProfileEditProfile[1]);
-  //   // screen.debug();
-  //   expect(screen.queryByText("Global").textContent).toMatch(/global/i);
-  //   expect(screen.queryByText("Chats").textContent).toMatch(/chats/i);
-  //   expect(screen.queryByText("Groups").textContent).toMatch(/groups/i);
-  //   expect(screen.queryAllByText("Profile")[0].textContent).toMatch(/profile/i);
-  //   expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
-  //   expect(screen.queryByText("Manage Profile").textContent).toMatch(
-  //     /manage profile/i,
-  //   );
-  //   expect(screen.queryAllByText("Profile")[1].textContent).toMatch(/profile/i);
-  //   expect(screen.queryByText("Edit Profile").textContent).toMatch(
-  //     /edit profile/i,
-  //   );
-  //   expect(screen.queryByText("Change Password").textContent).toMatch(
-  //     /change password/i,
-  //   );
-  //   expect(screen.queryByText("Profile Picture").textContent).toMatch(
-  //     /profile picture/i,
-  //   );
-  //   expect(screen.queryByText("Edit").textContent).toMatch(/edit/i);
-  //   // expect(
-  //   //   screen.queryAllByRole("button", { name: "Send" })[0],
-  //   // ).toBeInTheDocument();
-  //   expect(screen.queryByText("First name:").textContent).toMatch(
-  //     /first name:/i,
-  //   );
-  //   expect(
-  //     screen.queryByText("First name must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/first name must be between 1 and 30 characters/i);
-  //   expect(screen.queryByText("Last name:").textContent).toMatch(/last name:/i);
-  //   expect(
-  //     screen.queryByText("Last name must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/last name must be between 1 and 30 characters/i);
-  //   expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
-  //   expect(
-  //     screen.queryByText("Username must be between 1 and 30 characters")
-  //       .textContent,
-  //   ).toMatch(/username must be between 1 and 30 characters/i);
-  //   expect(screen.queryByText("Bio:").textContent).toMatch(/bio:/i);
-  //   expect(
-  //     screen.queryByText("Bio must be between 1 and 150 characters")
-  //       .textContent,
-  //   ).toMatch(/bio must be between 1 and 150 characters/i);
-  //   expect(
-  //     screen.queryByRole("button", { name: "save changes" }),
-  //   ).toBeInTheDocument();
-  // });
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    await user.type(screen.getByTestId("username"), "preslaw");
+
+    expect(screen.getByTestId("username")).toHaveValue("preslaw");
+
+    await user.type(screen.getByTestId("password"), "12345678Bg@");
+
+    expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
+
+    const submitBtn = screen.queryAllByRole("button");
+
+    await user.click(submitBtn[1]);
+
+    screen.debug();
+
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+    expect(screen.queryByText("Global").textContent).toMatch(/global/i);
+
+    expect(screen.queryByText("Chats").textContent).toMatch(/chats/i);
+
+    expect(screen.queryByText("Groups").textContent).toMatch(/groups/i);
+
+    expect(screen.queryAllByText("Profile")[0].textContent).toMatch(/profile/i);
+
+    expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
+
+    expect(screen.queryByText("Manage Profile").textContent).toMatch(
+      /manage profile/i,
+    );
+
+    expect(screen.queryAllByText("Profile")[1].textContent).toMatch(/profile/i);
+
+    expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
+      /edit profile/i,
+    );
+
+    expect(screen.queryByText("Change Password").textContent).toMatch(
+      /change password/i,
+    );
+
+    // expect(screen.queryByRole("button", { name: "Save" })).toBeInTheDocument();
+
+    expect(screen.queryByText("preslaw preslaw").textContent).toMatch(
+      /preslaw preslaw/i,
+    );
+
+    expect(screen.queryByText("@preslaw").textContent).toMatch(/@preslaw/);
+
+    expect(screen.queryByTestId("user_presence")).toBeInTheDocument();
+
+    expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
+      /edit profile/i,
+    );
+  });
+
+  it("should render Login form then navigate to EditUserProfile", async () => {
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/login", "/profile/4", "/profile/edit/4"],
+      initialIndex: 0,
+    });
+
+    server.use(
+      http.get("http://localhost:5000/users", () => {
+        return HttpResponse.json(
+          {
+            id: 4,
+            first_name: "preslaw",
+            last_name: "preslaw",
+            username: "preslaw",
+            password: "12345678Bg@",
+            confirm_password: "12345678Bg@",
+            bio: "",
+          },
+          { status: 200 },
+        );
+      }),
+    );
+
+    render(<RouterProvider router={router} />);
+
+    const user = userEvent.setup();
+
+    await user.type(screen.getByTestId("username"), "preslaw");
+
+    expect(screen.getByTestId("username")).toHaveValue("preslaw");
+
+    await user.type(screen.getByTestId("password"), "12345678Bg@");
+
+    expect(screen.getByTestId("password")).toHaveValue("12345678Bg@");
+
+    const submitBtn = screen.queryAllByRole("button");
+    // screen.debug();
+
+    await user.click(submitBtn[1]);
+
+    // const loadingBtn = await screen.findByTestId("loading-btn");
+
+    // expect(loadingBtn).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(() => screen.queryByText("Loading..."));
+
+    expect(screen.queryAllByText("Edit Profile")[1].textContent).toMatch(
+      /edit profile/i,
+    );
+
+    const manageUserProfileEditProfile = screen.queryAllByText("Edit Profile");
+
+    await user.click(manageUserProfileEditProfile[1]);
+
+    // screen.debug();
+
+    expect(screen.queryByText("Global").textContent).toMatch(/global/i);
+
+    expect(screen.queryByText("Chats").textContent).toMatch(/chats/i);
+
+    expect(screen.queryByText("Groups").textContent).toMatch(/groups/i);
+
+    expect(screen.queryAllByText("Profile")[0].textContent).toMatch(/profile/i);
+
+    expect(screen.queryByText("Logout").textContent).toMatch(/logout/i);
+
+    expect(screen.queryByText("Manage Profile").textContent).toMatch(
+      /manage profile/i,
+    );
+    expect(screen.queryAllByText("Profile")[1].textContent).toMatch(/profile/i);
+
+    expect(screen.queryByText("Edit Profile").textContent).toMatch(
+      /edit profile/i,
+    );
+
+    expect(screen.queryByText("Change Password").textContent).toMatch(
+      /change password/i,
+    );
+
+    expect(screen.queryByText("Profile Picture").textContent).toMatch(
+      /profile picture/i,
+    );
+
+    expect(screen.queryByText("Edit").textContent).toMatch(/edit/i);
+    // expect(
+    //   screen.queryAllByRole("button", { name: "Send" })[0],
+    // ).toBeInTheDocument();
+
+    expect(screen.queryByText("First name:").textContent).toMatch(
+      /first name:/i,
+    );
+
+    expect(
+      screen.queryByText("First name must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/first name must be between 1 and 30 characters/i);
+
+    expect(screen.queryByText("Last name:").textContent).toMatch(/last name:/i);
+
+    expect(
+      screen.queryByText("Last name must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/last name must be between 1 and 30 characters/i);
+
+    expect(screen.queryByText("Username:").textContent).toMatch(/username:/i);
+
+    expect(
+      screen.queryByText("Username must be between 1 and 30 characters")
+        .textContent,
+    ).toMatch(/username must be between 1 and 30 characters/i);
+
+    expect(screen.queryByText("Bio:").textContent).toMatch(/bio:/i);
+
+    expect(
+      screen.queryByText("Bio must be between 1 and 150 characters")
+        .textContent,
+    ).toMatch(/bio must be between 1 and 150 characters/i);
+
+    expect(
+      screen.queryByRole("button", { name: "save changes" }),
+    ).toBeInTheDocument();
+  });
 
   it("should navigate to EditProfile update the user information and render it", async () => {
     const router = createMemoryRouter(routes, {
