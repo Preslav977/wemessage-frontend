1654b84401da95ab62c5d17e185941fcb834a923
Preslav977 <p.cvetanow@gmail.com>
2025-03-06 08:59:17 +0200

    Solved the problem with scrolling everything, instead of the container in the RenderChatDetailsMessages using max-height and overflow

--- src/components/RenderChatDetailsMessages.jsx
+++ src/components/RenderChatDetailsMessages.jsx
@@ -1,7 +1,5 @@
+import styles from "./RenderChatDetailsMessages.module.css";
 import useFetchSingleChatURL from "./api/custom hooks/useFetchSingleChatURL";
-import { useState, useContext, useEffect } from "react";
-import { ChatDetailsContext } from "../contexts/ChatsContext";
-import { useParams } from "react-router-dom";
 
 function RenderChatDetailsMessages({ renderChatsOrChatDetails }) {
   const { chatDetails, error, loading } = useFetchSingleChatURL();
@@ -20,20 +18,36 @@ function RenderChatDetailsMessages({ renderChatsOrChatDetails }) {
 
   if (!renderChatsOrChatDetails) {
     return <h5>Chats</h5>;
-  } else {
+  } else if (renderChatsOrChatDetails && chatDetails !== null) {
     return (
-      <>
-        <img src={chatDetails.user.profile_picture} alt="" />
-        <h5>
-          {chatDetails.user.first_name} {chatDetails.user.last_name}
-        </h5>
+      <div className={styles.chatDetailsContainer}>
+        <div className={styles.chatDetailsUserContainer}>
+          <img src={chatDetails.user.profile_picture} alt="" />
+          <h5 className={styles.test}>
+            {chatDetails.user.first_name} {chatDetails.user.last_name}
+          </h5>
+        </div>
         <hr />
         {chatDetails.messages.length === 0 ? (
           <p>Start a conversation, say Hi!</p>
         ) : (
-          ""
+          <div className={styles.chatDetailsMessageContainer}>
+            <ul className={styles.ulMessageContainer}>
+              {chatDetails.messages.map((message) => (
+                <li key={message.id} className={styles.sendMessage}>
+                  {message.message_text}
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
-      </>
+        {/* <hr /> */}
+        <div className={styles.sendMessageInput}>
+          <input type="text" name="" id="" />
+          <input type="file" name="" id="" />
+          <button type="submit">Send</button>
+        </div>
+      </div>
     );
   }
 }
