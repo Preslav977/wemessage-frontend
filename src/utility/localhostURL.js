2cae70f32d33bb771aaa927db79afc33fd95d864
Preslav977 <p.cvetanow@gmail.com>
2025-04-27 14:18:06 +0300

    Deployed to project to vercel changed from localhost to deployed URL

--- src/utility/localhostURL.js
+++ src/utility/localhostURL.js
@@ -1,4 +1,7 @@
 //this will replace all currently used localhost URLs in order to make it easier to swap later
-const localhostURL = "http://localhost:5000";
+// const localhostURL = "http://localhost:5000";
+
+const localhostURL =
+  "wemessage-frontend-eq1v0vrsw-preslav977s-projects.vercel.app";
 
 export default localhostURL;
